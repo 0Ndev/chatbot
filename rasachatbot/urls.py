5d72b55764b992c1447afc9b2d8ccb5d58e9880f
natalialexandra <natalialllepost@gmail.com>
2021-05-19 11:50:00 +0200

    chatbot

---
+++ rasachatbot/urls.py
@@ -0,0 +1,27 @@
+"""rasachatbot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from chatbot import views
+import chatbot
+from django.contrib import admin
+from django.urls import path, include
+from chatbot import views
+from chatbot import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('chatroom/', include(chatbot.urls)),
+]
